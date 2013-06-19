fd804dd76e31cf1645344db6ae680e88dfb9e4b4
James Kirsop <james.kirsop@gmail.com>
2013-06-19 17:55:37 +1000

    Cleanup, Documentation and Copyright Notice
    Added some documentation, copyright and removed some unnecessary
    in-line comments

--- grapher.js
+++ grapher.js
@@ -1,9 +1,22 @@
 /*
-	By James Kirsop - 2013
-	Functions for Extending the functionality of Raphael to generate graphs
+	Grapher.js
+	Simple functions for Extending the functionality of Raphael to generate graphs
+	Copyright 2013 James Kirsop
+	www.jameskirsop.com
+	Email Me: james.kirsop@gmail.com
 */
 	var aColors = new Array("b80043", "c85437", "32b9cd", "b44bb0", "ffea00", "b292d3", "2fd0c8", "face0a", "2267dd", "39c64c", "ff6600", "f20733", "25dacb", "c23d6e", "e718e3", "01feb2", "4cc33c", "51d8f0", "5cdf11", "00eaff", "ac21de", "ff0000", "33b2cc", "4330cf");
-	
+	var sStrokeColor = '#B6B6B6';
+
+	/*
+		drawGraph()
+		Takes the following arguments:
+		containerWidth -  Integer - The width (in pixels) of the parent canvas
+		containerHeight - Integer - The height (in pixels) of the parent canvas
+		labels - Array - The text descriptions of each of the values in the 'values' array. 
+		values - Array - Required to be numerical values
+		gutter - Integer - How much space (in pixels) is required around the graph to fit the labels in on both the left vertical and bottom horizontal axis
+	*/
 	Raphael.fn.drawGraph = function(containerWidth,containerHeight,labels,values,gutter){
 		var container = this.rect(0,0,containerWidth,containerHeight);
 		container.attr({"stroke-width":0});
@@ -18,9 +31,6 @@
 			maxValue++;
 		}
 		var verticalScale = height/maxValue;
-
-		// alert(Math.max.apply(Math,values));
-		// alert(verticalScale);
 		var pointHorizontalDistance=width/(values.length-1);
 
 		/* Horizontal Axis Labels */
@@ -47,6 +57,56 @@
 		vector.animate({path:"M"+gutter+","+(ticketNumbersGraph[0]+gutter)+plotString});
 	}
 
+	/* drawPie 
+	* Takes the following arguments:
+	* containerWidth = Width of the element in number of pixels the pie chart will reside in
+	* containerHeight = Height of the element in number of pixels the pie chart will reside in
+	* radius = How large, in pixels, the radius of the pie chart will be 
+	* values = an Array of the values in the order in which they are to be displayed
+	* labels = an Array of labels, that match the order of values in which they are to be assigned to segments of the chart
+	*/
+	Raphael.fn.drawPie = function(containerWidth, containerHeight, radius, values, labels){
+		var totalOfValues = 0;
+		for (var i = 0; i <= values.length-1; i++) {
+			totalOfValues = totalOfValues + (values[i] * 1);
+		};
+		var iOriginX = (containerWidth/2);
+		var iOriginY = containerHeight/2;
+		var prevDegree = 0;
+		var valuesCount = 0;
+		var bigBalloon = 0;
+		for (var i = 0; i <= values.length-1; i++) {
+			var colorIndex = Math.floor(Math.random() * (aColors.length - 1 +1) + 0);
+			valuesCount = valuesCount + (values[i] * 1);
+			var thisDegree = (valuesCount/totalOfValues) * 360;
+			path=this.path("M"+iOriginX+" "+iOriginY+"L"
+				+(iOriginX+(radius*Math.cos(thisDegree * Math.PI/180)))+" "
+				+(iOriginY+(radius*Math.sin(thisDegree * Math.PI/180)))+"A"+radius+" "+radius+" 0 "+bigBalloon+" 0 "
+				+(iOriginX+(radius*Math.cos(prevDegree * Math.PI/180)))+" "
+				+(iOriginY+(radius*Math.sin(prevDegree * Math.PI/180)))+" L"+iOriginX+" "+iOriginY);
+			path.attr({"fill":"#"+aColors[i],"stroke":"#C0C0C0","stroke-width":1});
+			addX = 0;
+			addY = 0;
+			if (thisDegree <= 45 || thisDegree >= 315) {
+				addX = radius/2;
+			} else if (thisDegree >= 135 && thisDegree <= 225) {
+				addX = -radius/2;
+			};
+			if (thisDegree >= 45 && thisDegree <= 135) {
+				addY = radius/5;
+			} else if (thisDegree >= 250 && thisDegree <= 305) {
+				addY = -radius/5;
+			};
+			if((i*35)<=containerHeight){
+				var key = this.rect(iOriginX+(radius*1.5)-20,iOriginY-radius+(21*(i))-5,10,10,0);
+				key.attr({"fill":aColors[i],"stroke-width":0});
+				var text = this.text(iOriginX+(radius*1.5),iOriginY-radius+(21*(i)),labels[i]+ " - "+values[i]);
+				text.attr({"font-size":15,"text-anchor":"start"});
+				var prevDegree = (valuesCount/totalOfValues) * 360;
+			}
+		};
+	}
+
 	/* Raphael function to draw a grid for the graph */
 	Raphael.fn.drawGrid = function(width,height,cols,rows,gutter){
 		var wInterval = width/(cols-1);
@@ -62,56 +122,4 @@
 			path=this.path("M"+((i*wInterval)+gutter)+","+gutter+"V"+(height+gutter));
 			path.attr("stroke","#818181");
 		}
-	}
-
-	/* drawPie 
-	* Takes the following arguments:
-	* containerWidth = Width of the element in number of pixels the pie chart will reside in
-	* containerHeight = Height of the element in number of pixels the pie chart will reside in
-	* radius = How large, in pixels, the radius of the pie chart will be 
-	* values = an Array of the values in the order in which they are to be displayed
-	* labels = an Array of labels, that match the order of values in which they are to be assigned to segments of the chart
-	*/
-	Raphael.fn.drawPie = function(containerWidth, containerHeight, radius, values, labels){
-		//var colours = new Array("4D7FFF","19C919","FFC508","7F65C2","FF3A3A","3BD8FF","AF9BFF","19C919","FFC508","7F65C2","FF3A3A","3BD8FF","FF00CC","FFE100","3349B6");
-		var colours = aColors;
-		var totalOfValues = 0;
-		for (var i = 0; i <= values.length-1; i++) {
-			totalOfValues = totalOfValues + (values[i] * 1);
-		};
-		var iOriginX = (containerWidth/2);
-		var iOriginY = containerHeight/2;
-		var prevDegree = 0;
-		var valuesCount = 0;
-		var bigBalloon = 0;
-		for (var i = 0; i <= values.length-1; i++) {
-			var colorIndex = Math.floor(Math.random() * (colours.length - 1 +1) + 0);
-			valuesCount = valuesCount + (values[i] * 1);
-			var thisDegree = (valuesCount/totalOfValues) * 360;
-			path=this.path("M"+iOriginX+" "+iOriginY+"L"
-				+(iOriginX+(radius*Math.cos(thisDegree * Math.PI/180)))+" "
-				+(iOriginY+(radius*Math.sin(thisDegree * Math.PI/180)))+"A"+radius+" "+radius+" 0 "+bigBalloon+" 0 "
-				+(iOriginX+(radius*Math.cos(prevDegree * Math.PI/180)))+" "
-				+(iOriginY+(radius*Math.sin(prevDegree * Math.PI/180)))+" L"+iOriginX+" "+iOriginY);
-			path.attr({"fill":"#"+colours[i],"stroke":"#C0C0C0","stroke-width":1});
-			addX = 0;
-			addY = 0;
-			if (thisDegree <= 45 || thisDegree >= 315) {
-				addX = radius/2;
-			} else if (thisDegree >= 135 && thisDegree <= 225) {
-				addX = -radius/2;
-			};
-			if (thisDegree >= 45 && thisDegree <= 135) {
-				addY = radius/5;
-			} else if (thisDegree >= 250 && thisDegree <= 305) {
-				addY = -radius/5;
-			};
-			if((i*35)<=containerHeight){
-				var key = this.rect(iOriginX+(radius*1.5)-20,iOriginY-radius+(21*(i))-5,10,10,0);
-				key.attr({"fill":colours[i],"stroke-width":0});
-				var text = this.text(iOriginX+(radius*1.5),iOriginY-radius+(21*(i)),labels[i]+ " - "+values[i]);
-				text.attr({"font-size":15,"text-anchor":"start"});
-				var prevDegree = (valuesCount/totalOfValues) * 360;
-			}
-		};
 	}
